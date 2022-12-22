9a8c570d610eff3678fbcfb1620c7c0224fd1ec9
plutoLam <1244715423@qq.com>
2022-12-22 11:37:47 +0800

    feat: 增加monorepo分包

---
+++ packages/hooks/use-namespace/index.ts
@@ -0,0 +1,117 @@
+import { computed, unref } from "vue";
+// 命名前缀
+export const defaultNamespace = "pl";
+const statePrefix = "is-";
+// BEM 命名字符拼接函数
+const _bem = (
+  namespace: string,
+  block: string,
+  blockSuffix: string,
+  element: string,
+  modifier: string
+) => {
+  // 默认是 Block
+  let cls = `${namespace}-${block}`;
+  // 如果存在 Block 前缀，也就是 Block 里面还有 Block，例如：el-form 下面还有一个 el-form-item
+  if (blockSuffix) {
+    cls += `-${blockSuffix}`;
+  }
+  // 如果存在元素
+  if (element) {
+    cls += `__${element}`;
+  }
+  // 如果存在修改器
+  if (modifier) {
+    cls += `--${modifier}`;
+  }
+  // console.log('cls: ', cls);
+  return cls;
+};
+
+export const useNamespace = (block: string) => {
+  // 命名前缀也就是命名空间
+  const namespace = computed(() => defaultNamespace);
+  // 创建块 例如：el-form
+  const b = (blockSuffix = "") =>
+    _bem(unref(namespace), block, blockSuffix, "", "");
+  // 创建元素 例如：el-input__inner
+  const e = (element?: string) =>
+    element ? _bem(unref(namespace), block, "", element, "") : "";
+  // 创建块修改器 例如：el-form--default
+  const m = (modifier?: string) =>
+    modifier ? _bem(unref(namespace), block, "", "", modifier) : "";
+  // 创建前缀块元素 例如：el-form-item
+  const be = (blockSuffix?: string, element?: string) =>
+    blockSuffix && element
+      ? _bem(unref(namespace), block, blockSuffix, element, "")
+      : "";
+  // 创建元素修改器 例如：el-scrollbar__wrap--hidden-default
+  const em = (element?: string, modifier?: string) =>
+    element && modifier
+      ? _bem(unref(namespace), block, "", element, modifier)
+      : "";
+  // 创建块前缀修改器 例如：el-form-item--default
+  const bm = (blockSuffix?: string, modifier?: string) => {
+    // console.log("modifier: ", modifier);
+    // console.log("blockSuffix: ", blockSuffix);
+    return blockSuffix && modifier
+      ? _bem(unref(namespace), block, blockSuffix, "", modifier)
+      : "";
+  };
+  // 创建块元素修改器 例如：el-form-item__content--xxx
+  const bem = (blockSuffix?: string, element?: string, modifier?: string) =>
+    blockSuffix && element && modifier
+      ? _bem(unref(namespace), block, blockSuffix, element, modifier)
+      : "";
+  // 动作状态 例如：is-success is-required
+  // 如果没有传入第二个参数，则默认这个状态一直存在
+  const is = (name: string, ...args: [boolean | undefined] | []) => {
+    const state = args.length >= 1 ? args[0]! : true;
+    return name && state ? `${statePrefix}${name}` : "";
+  };
+
+  // for css var
+  // --el-xxx: value;
+  const cssVar = (object: Record<string, string>) => {
+    const styles: Record<string, string> = {};
+    for (const key in object) {
+      if (object[key]) {
+        styles[`--${namespace.value}-${key}`] = object[key];
+      }
+    }
+    return styles;
+  };
+  // with block
+  const cssVarBlock = (object: Record<string, string>) => {
+    const styles: Record<string, string> = {};
+    for (const key in object) {
+      if (object[key]) {
+        styles[`--${namespace.value}-${block}-${key}`] = object[key];
+      }
+    }
+    return styles;
+  };
+
+  const cssVarName = (name: string) => `--${namespace.value}-${name}`;
+  const cssVarBlockName = (name: string) =>
+    `--${namespace.value}-${block}-${name}`;
+
+  return {
+    namespace,
+    b,
+    e,
+    m,
+    be,
+    em,
+    bm,
+    bem,
+    is,
+    // css
+    cssVar,
+    cssVarName,
+    cssVarBlock,
+    cssVarBlockName,
+  };
+};
+
+export type UseNamespaceReturn = ReturnType<typeof useNamespace>;
